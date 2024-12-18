a4fbb472bd3da9705ba688f51138d8b64bc0ccf1
dmitry.lahutkin <dmitry.lahutkin@humans.tech>
2024-12-18 11:53:18 +0100

    feat: routing for articles

---
+++ src/constants/staticPaths.ts
@@ -0,0 +1,5 @@
+export const STATIC_PATHS = [
+  { title: "Citocheratina", slug: "article-1" },
+  { title: "Some title", slug: "article-2" },
+  // Add more articles or slugs here
+];
--- src/scripts/parseHtml.js
+++ src/scripts/parseHtml.js
@@ -3,8 +3,8 @@ import path from "path";
 import * as cheerio from "cheerio";
 
 // Function to parse the HTML file
-export async function parseHtml() {
-  const filePath = path.resolve("./src/content/article-1.html");
+export async function parseHtml({ slug }) {
+  const filePath = path.resolve(`./src/content/${slug}.html`);
   const htmlContent = fs.readFileSync(filePath, "utf-8");
 
   const $ = cheerio.load(htmlContent);
